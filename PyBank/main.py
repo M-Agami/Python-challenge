0d298d7f17d31558182e8358335fb162a8ed1ad0
Maury Agami <62514160+M-Agami@users.noreply.github.com>
2020-05-29 20:45:56 -0400

    added basic functions for variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,5 +6,17 @@ import csv
 with open(file, 'r') as text:
     print(text)
     budget_list = text.read()
-    print (budget_list)
-    
+   
+    #declare variables used to store data
+total_months = 0
+net_total = 0
+avg_change = 0
+greatest_inc = 0
+greatest_dec = 0
+    for row in budget_list:
+        next row
+        total_months = len(budget_list)
+        net_total = row[1]
+        avg_change = net_total/total_months
+        greatest_inc = budget_list.max(row[2])
+        greatest_dec = budget_list.max(row[2])
