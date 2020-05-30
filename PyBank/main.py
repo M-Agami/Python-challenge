98a97e1d0d1a84b18c7467a83957ab7f74198914
Maury Agami <mauricioagami@live.com>
2020-05-30 17:34:38 -0400

    added base functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,9 +3,11 @@ file = '/Users/Maury/Documents/GitHub/Python-challenge/PyBank/Resources/budget_d
 import os
 import csv
 
-with open(file, 'r') as text:
+with open(file, 'r', newline= '') as csvtext:
+    csvtext= csv.reader
     print(text)
-    budget_list = text.read()
+    budget_list = csvtext.read()
+    
    
     #declare variables used to store data
 total_months = 0
@@ -13,10 +15,25 @@ net_total = 0
 avg_change = 0
 greatest_inc = 0
 greatest_dec = 0
-    for row in budget_list:
-        next row
-        total_months = len(budget_list)
-        net_total = row[1]
-        avg_change = net_total/total_months
-        greatest_inc = budget_list.max(row[2])
-        greatest_dec = budget_list.max(row[2])
+
+for row in budget_list:
+    next.reader
+    total_months = len(budget_list)
+    net_total = row[1]
+    avg_change = net_total/total_months
+    greatest_inc = budget_list.max(row[2])
+    greatest_dec = budget_list.max(row[2])
+    analyzed_budget_list = {["Total months":total_months]
+                            ,["Net total": net_total]
+                            ,["Average change": avg_change]
+                            ,["Greatest increase": greatest_inc]
+                            ,["Greatest decrease": greatest_dec]
+                            }
+    print (analyzed_budget_list)
+        
+        
+
+
+
+with open(analyzed_budget_list, "w") as txt_file:
+    txt_file.write(Analysis/Analyzed_budget_list.txt)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,10 +2,37 @@ file = '/Users/Maury/Documents/GitHub/Python-challenge/PyPoll/Resources/election
 
 import os
 import csv
+import collections
 
-with open(file, 'r') as text:
-    print(text)
-    election_list = text.read()
+with open(file, 'r', newline='') as csvtext:
+    csvtext= csv.reader(csvtext, delimiter = ',')
+    print(csvtext)
+    election_list = csvtext.read()
     
-    
-    
+#declare variables to store data
+total_votes = 0
+candidates = []
+percent_won = 0
+candidate_total_won = []
+winner = []
+
+#start loop
+for row in election_list:
+    next.reader
+    total_votes = len(election_list)
+    candidate_list = set(field.strip().lower() for field in row[2])
+    candidate_list = item[row[0]].append(row[1]) # way 2
+    for item in candidate_list:
+            if item not in candidates:
+                candidates.append(item)
+    for k, v in candidate_list.items(): #way 2
+        print(k)
+    candidate_total_won = Counter(v)
+
+print(f"Total votes: " {total_votes})
+print(f"Candidates: " {candidates})
+print(f"{candidates[0]})
+
+
+with open(analyzed_election_list, "w") as txt_file:
+    txt_file.write(Analysis/Analyzed_election_list.txt)
