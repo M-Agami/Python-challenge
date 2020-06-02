f534e5e554082ef8de6ce6ae832c8780e8b653ad
Maury Agami <mauricioagami@live.com>
2020-06-01 21:07:36 -0400

    added code  and correctly defined output path/ file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,38 +1,51 @@
-file = '/Users/Maury/Documents/GitHub/Python-challenge/PyPoll/Resources/election_data.csv'
-
 import os
 import csv
 import pathlib
 
 input_file = Path("..","Resources","election_data.csv")
-with open(input_file, 'r', newline='') as csvtext:
-    csvtext= csv.reader(csvtext, delimiter = ',')
-    election_list = csvtext.read()
-    
+
 #declare variables to store data
 total_votes = 0
-candidates = []
+khan = 0
+correy = 0
+li = 0
+o_tooley = 0
 percent_won = 0
-candidate_total_won = []
-winner = []
+
+#start loop
+with open(input_file, 'r', newline='', encoding = utf8) as csvtext:
+    csvtext= csv.reader(csvtext, delimiter = ',')
+    election_list = csvtext.read()
+    next.election_list
 
 #start loop
 for row in election_list:
-    next.reader
     total_votes = len(election_list)
-    candidate_list = set(field.strip().lower() for field in row[2])
-    candidate_list = item[row[0]].append(row[1]) # way 2
     for item in candidate_list:
             if item not in candidates:
-                candidates.append(item)
-    for k, v in candidate_list.items(): #way 2
-        print(k)
-    candidate_total_won = Counter(v)
+                candidates.append(row[2])
+                
+if row[2] == "Khan": 
+        khan +=1
+    elif row[2] == "Correy":
+        correy +=1
+    elif row[2] == "Li": 
+        li +=1
+    elif row[2] == "O'Tooley":
+        o_tooley +=1
 
-print(f"Total votes: " {total_votes})
-print(f"Candidates: " {candidates})
-print(f"{candidates[0]})
+print(f"Total votes: {total_votes}\n ---------------")
+print(f"votes Khan:{khan}\n Percent of votes: {khan / total_votes * 100}% \n ")
+print(f"votes Correy: {correy}\n Percent of votes: {correy / total_votes * 100}% \n")
+print(f"votes li: {li}\n Percent of votes: {li / total_votes * 100}% \n")
+print(f"votes O'Tooley: {o_tooley}\n Percent of votes: {o_tooley / total_votes * 100}% \n)
 
-
-with open(analyzed_election_list, "w") as txt_file:
-    txt_file.write(Analysis/Analyzed_election_list.txt)
+output_file = Path("python-challenge", "PyPoll", "Analyzed_election_list.txt")
+Analyzed_election_list = output_file
+with open(Analyzed_election_list, "w") as txt_file:
+    txt_file.write(f'Total votes {total_votes} \n')
+    txt_file.write(f'Votes for Khan {khan}')
+    txt_file.write(f'Votes for Correy {correy}\n')
+    txt_file.write(f'Votes for Li {li} \n')
+    txt_file.write(f"votes for O'Tooley {o_tooley}\n")
+    txt_file.write(f'Percent of votes won 
